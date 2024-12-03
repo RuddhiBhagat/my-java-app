55571543c1a6ed5d6658112b683791df3c4d078e
RuddhiBhagat <ruddhi.bhagat@atyeti.com>
2024-12-03 17:48:16 +0530

    Pushing changes

--- App.java
+++ App.java
@@ -2,6 +2,13 @@
 
 public class App {
     public static void main(String[] args) {
+        // Retrieve environment variables from .env file
+        String myVariable = EnvConfig.get("MY_VARIABLE");
+        String mySecretKey = EnvConfig.get("MY_SECRET_KEY");
+
+        // Print out the values to confirm they're being loaded
+        System.out.println("MY_VARIABLE: " + myVariable);
+        System.out.println("MY_SECRET_KEY: " + mySecretKey);
         System.out.println("Hello,World!");
     }
 }
